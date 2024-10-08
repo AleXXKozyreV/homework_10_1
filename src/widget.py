7582120dd1f7e03be54142b2fa8a0eef1ef589fe
Aleksey Kozyrev <alexxkozyrev@ya.ru>
2024-10-09 02:17:12 +0300

    widget fixed

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,6 @@
 from src.masks import get_mask_account, get_mask_card_number
 
+
 def mask_account_card(card_or_acc_num: str) -> str:
     """Функция принимает тип и номер карты или счета в виде строки и возвращает строку с замаскированным номером"""
 
@@ -11,4 +12,12 @@ def mask_account_card(card_or_acc_num: str) -> str:
         mask_account_ = get_mask_account(card_or_acc_num.split()[-1])
         result = f"{card_or_acc_num[:-20]}{mask_account_}"
 
+    return result
+
+
+def get_date(old_data: str) -> str:
+    """Функция принимает строку с датой и форматирует её"""
+
+    data_slice = old_data[0:10].split("-")
+    result = ".".join(data_slice[::-1])
     return result
