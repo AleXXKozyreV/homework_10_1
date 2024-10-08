0f676ec3bb2a8d78150adb2b7cd881addd74d8e1
Aleksey Kozyrev <alexxkozyrev@ya.ru>
2024-10-09 00:01:00 +0300

    homework_9.1

---
+++ src/masks.py
@@ -0,0 +1,13 @@
+def get_mask_card_number(card_number: str) -> str:
+    """Функция принимает на вход номер карты в виде строки и возвращает её маску"""
+
+    blocks_card_numbers = [card_number[:4], card_number[4:6] + "**", "****", card_number[-4:]]
+    mask_card_number = " ".join(blocks_card_numbers)
+    return mask_card_number
+
+
+def get_mask_account(account_number: str) -> str:
+    """Функция принимает на вход номер счета в виде строки и возвращает его маску"""
+
+    mask_account = "**" + account_number[-4:]
+    return mask_account
