5faab73855cfbe15587817f5e1eb49d5c2784f4b
Aleksey Kozyrev <alexxkozyrev@ya.ru>
2024-10-27 13:17:27 +0300

    Fixed test_generators, index

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,3 +1,5 @@
+import pytest
+
 from src.generators import card_number_generator, filter_by_currency, transaction_descriptions
 
 
@@ -8,19 +10,30 @@ def test_filter_by_currency(transaction):
     assert next(transaction_filter)["id"] == 895315941
 
 
-def test_transaction_descriptions(transaction):
-    descriptions_generator = transaction_descriptions(transaction)
-    assert next(descriptions_generator) == "Перевод организации"
-    assert next(descriptions_generator) == "Перевод со счета на счет"
-    assert next(descriptions_generator) == "Перевод со счета на счет"
-    assert next(descriptions_generator) == "Перевод с карты на карту"
-    assert next(descriptions_generator) == "Перевод организации"
+@pytest.mark.parametrize('index, expected', [
+    (0, "Перевод организации"),
+    (1, "Перевод со счета на счет"),
+    (2, "Перевод со счета на счет"),
+    (3, "Перевод с карты на карту"),
+    (4, "Перевод организации")
+])
+def test_transaction_descriptions(index, expected):
+    transactions = [
+        {'description': "Перевод организации"},
+        {'description': "Перевод со счета на счет"},
+        {'description': "Перевод со счета на счет"},
+        {'description': "Перевод с карты на карту"},
+        {'description': "Перевод организации"}
+    ]
+    descriptions = list(transaction_descriptions(transactions))
+    assert descriptions[index] == expected
 
 
-def test_card_number_generator() -> None:
-    generator = card_number_generator(1, 5)
-    assert next(generator) == "0000 0000 0000 0001"
-    assert next(generator) == "0000 0000 0000 0002"
-    assert next(generator) == "0000 0000 0000 0003"
-    assert next(generator) == "0000 0000 0000 0004"
-    assert next(generator) == "0000 0000 0000 0005"
+@pytest.mark.parametrize('start, stop, result', [
+    (1, 4, ["0000 0000 0000 0001", "0000 0000 0000 0002", "0000 0000 0000 0003", "0000 0000 0000 0004"]),
+    (11223344, 11223345, ['0000 0000 1122 3344', '0000 0000 1122 3345']),
+    (1111222233334444, 1111222233334445, ['1111 2222 3333 4444', '1111 2222 3333 4445'])
+])
+def test_card_number_generator(start: int, stop: int, result: list) -> None:
+    generator = list(card_number_generator(start, stop))
+    assert generator == result
